1f20c083601b196cf0bc681fccc0187b0684af20
leoprietsch <leo.prietsch@gmail.com>
2022-02-04 19:03:33 -0300

    feat: add fileUpload middleware

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
 import express from "express";
+import fileUpload, { UploadedFile } from "express-fileupload";
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(fileUpload());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Initial setup");
+  if (!req.files || Object.keys(req.files).length === 0) {
+    res.status(400).send("No files were uploaded.");
+    return;
+  }
+
+  const file = req.files.file as UploadedFile;
+
+  res.send(file);
 });
 
 app.listen(port, () => {
