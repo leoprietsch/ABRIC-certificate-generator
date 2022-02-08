bfe890f88fcf2da7e7ea3467067f34a37f3286a5
leoprietsch <leo.prietsch@gmail.com>
2022-02-08 09:19:08 -0300

    feat: add parsing and define options for parsing .csv file

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
+import { parse } from "csv-parse";
 import express from "express";
 import fileUpload, { UploadedFile } from "express-fileupload";
+import Certificate from "./entities/Certificate";
 
 const app = express();
 const port = process.env.PORT;
@@ -7,14 +9,31 @@ const port = process.env.PORT;
 app.use(fileUpload());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.post("/", (req, res) => {
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).send("No files were uploaded.");
   }
 
   const file = req.files.file as UploadedFile;
 
-  res.send(file);
+  const headers = ["name", "project", "fair", "prize", "email"];
+
+  parse(
+    file.data,
+    {
+      columns: headers,
+      from: 2,
+      trim: true,
+    },
+    (error, results: Certificate[]) => {
+      if (error) {
+        console.error(error);
+        res.send(error);
+      }
+
+      res.send(results);
+    }
+  );
 });
 
 app.listen(port, () => {
